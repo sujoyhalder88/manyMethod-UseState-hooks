09ce408f9137fe6496502a6ed0d8565a148df851
sujoyhalder88 <102689295+sujoyhalder88@users.noreply.github.com>
2022-06-21 17:15:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import BasicForm from "./From/BasicForm";
+// import ShortCurcuit from "./Component/ShortCurcuit";
+// import UseStateObject from "./Component/UseStateObject";
+// import UseState from "./Component/UseState";
+// import UseStateArray from "./Component/UseStateArray";
+
+const App = () => {
+    return (
+        <div className="text">
+            {/* <UseStateArray /> */}
+            {/* <UseState /> */}
+            {/* <UseStateObject /> */}
+            {/* <ShortCurcuit /> */}
+            <BasicForm />
+        </div>
+    );
+};
+
+export default App;
